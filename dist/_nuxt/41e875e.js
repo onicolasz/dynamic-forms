b5edddfb5ff96748b9dc98b5999cea5980bbb235
Nicolas Barros <nicolas.souza@4ued.tech>
2023-12-03 19:50:31 -0300

    remove dist from .gitignore

---
+++ dist/_nuxt/41e875e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{333:function(n,t,e){"use strict";e.r(t);var l=e(48),component=Object(l.a)({},(function(){return(0,this._self._c)("h1",[this._v("Essa página ainda não existe")])}),[],!1,null,null,null);t.default=component.exports}}]);
